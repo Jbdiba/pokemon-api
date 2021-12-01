220b6fd7a99ddf04d3ffebc199f85cf17bd6ac4b
Diba <dibaja@ad.vzwcorp.com>
2021-11-30 18:18:43 -0600

    final

--- pokemon.js
+++ pokemon.js
@@ -57,3 +57,25 @@ fetchDogs("hound").then((data) => {
     console.log(data) 
 
 })
+
+// // example-4:
+
+// import fetch from "node-fetch";
+
+// const fetchBird = async (id) => {
+//     try {
+//         const res = await fetch (`https://audubon-api.herokuapp.com/api/birds/${id}`)
+//         const data = await res.json();
+//         const bird = {
+//             "name":data.name,
+//         }
+//         return (data)       
+//     } catch (err) {
+//         console.log(err)
+
+//     }
+// }
+
+// fetchBird("5f8ef8386b1fb90004bde060").then((data) => {
+//     console.log(data)
+// })
